0af3bfd8efbabae43f10777a8664a9d8f965d343
jeffersonkg745 <77896216+jeffersonkg745@users.noreply.github.com>
2022-01-10 18:49:54 -0500

    Corrected calendar header to be more user friendly

--- screens/UserCalendar.js
+++ screens/UserCalendar.js
@@ -3,48 +3,48 @@ import {
 	SafeAreaView,
 	StatusBar,
 	Text,
-    Button,
+	Button,
 	View
 } from 'react-native';
 import {
-    Calendar
+	Calendar
 } from 'react-native-calendars';
 
 
 
 
-export default function UserCalendar({ navigation}) {
-	
+export default function UserCalendar({ navigation }) {
+
 	return (
-	<SafeAreaView >
-		<StatusBar style="auto" />
-		<Text>Calendar Screen</Text>
-		<Button
-			title="Go Home"
-			onPress={() => navigation.navigate("Home")}
-		>
-		</Button>
-		<View >
-			<Calendar
-				current={'2022-01-08'}
-				minDate={'2022-01-01'}
-				maxDate={'2023-01-31'}
-				onDayPress={day => {
-					console.log('selected day', day);
-				}}
-				monthFormat={'yyyy MM'}
-				onMonthChange={month => {
-					console.log('month changed', month);
-				}}
-				hideExtraDays={true}
-				firstDay={1}
-				onPressArrowLeft={subtractMonth => subtractMonth()}
-				onPressArrowRight={addMonth => addMonth()}
-				disableAllTouchEventsForDisabledDays={true}
+		<SafeAreaView >
+			<StatusBar style="auto" />
+			<Text>Calendar Screen</Text>
+			<Button
+				title="Go Home"
+				onPress={() => navigation.navigate("Home")}
+			>
+			</Button>
+			<View >
+				<Calendar
+					current={'2022-01-08'}
+					minDate={'2022-01-01'}
+					maxDate={'2023-01-31'}
+					onDayPress={day => {
+						console.log('selected day', day);
+					}}
+					monthFormat={'MMMM yyyy'}
+					onMonthChange={month => {
+						console.log('month changed', month);
+					}}
+					hideExtraDays={true}
+					firstDay={1}
+					onPressArrowLeft={subtractMonth => subtractMonth()}
+					onPressArrowRight={addMonth => addMonth()}
+					disableAllTouchEventsForDisabledDays={true}
 				/>
-		</View>
-		
-	</SafeAreaView>
+			</View>
+
+		</SafeAreaView>
 	);
 };
 
