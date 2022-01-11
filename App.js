3565be7191dd2728f6a553e9546a42b1f6cd7c8f
Matt Faucher <51377027+mattfaucher@users.noreply.github.com>
2022-01-11 16:58:00 -0500

    added src dir to clean up file structure

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import Home from './screens/Home.js';
-import Settings from './screens/Settings.js';
-import UserCalendar from './screens/UserCalendar.js';
+import Home from './src/screens/Home.js';
+import Settings from './src/screens/Settings.js';
+import UserCalendar from './src/screens/UserCalendar.js';
 
 const Tab = createBottomTabNavigator();
 
