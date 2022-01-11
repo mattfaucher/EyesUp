b4d48a52e03072918cfdbfffe4340b57d85476be
Matt Faucher <51377027+mattfaucher@users.noreply.github.com>
2022-01-11 15:46:25 -0500

    trying to add vertical line separators between icons

--- App.js
+++ App.js
@@ -6,53 +6,58 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import Home from './screens/Home.js';
 import Settings from './screens/Settings.js';
 import UserCalendar from './screens/UserCalendar.js';
-import { tintColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <>
       <StatusBar style='auto'></StatusBar>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          tabBarActiveTintColor: '#007AFF',
-          tabBarInactiveTintColor: 'grey',
-          tabBarShowLabel: false,
-        }}
-      >
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            tabBarIcon: ({ color }) => (
-              <Icon name="hourglass" size={30} color={color} />
-            )
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            tabBarActiveTintColor: '#007AFF',
+            tabBarInactiveTintColor: 'grey',
+            tabBarShowLabel: false,
+            tabBarStyle: {
+              height: 100,
+              paddingBottom: 30
+            }
           }}
-        />
-        <Tab.Screen
-          name="Calendar"
-          component={UserCalendar}
-          options={{
-            tabBarLabel: 'Calendar',
-            tabBarIcon: ({ color }) => (
-              <Icon name="calendar" size={30} color={color} />
-            )
-          }}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={Settings}
-          options={{
-            tabBarLabel: 'Settings',
-            tabBarIcon: ({ color }) => (
-              <Icon name="sliders" size={30} color={color} />
-            )
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+        >
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              tabBarIcon: ({ color }) => (
+                <Icon name="hourglass" size={30} color={color} />
+              )
+            }}
+          />
+          <Tab.Screen
+            name="Calendar"
+            component={UserCalendar}
+            options={{
+              tabBarLabel: 'Calendar',
+              tabBarIcon: ({ color }) => (
+                <Icon name="calendar" size={30} color={color} />
+              )
+            }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={Settings}
+            options={{
+              tabBarLabel: 'Settings',
+              tabBarIcon: ({ color }) => (
+                <Icon name="sliders" size={30} color={color} />
+              )
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
